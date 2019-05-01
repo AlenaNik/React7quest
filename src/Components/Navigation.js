ada9f02f9a72613feb999d76b38f4e6585899d66
AlenaNik <alena.nik.digital@gmail.com>
2019-05-01 10:58:56 +0200

    color red for history button when active

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -5,7 +5,7 @@ const Navigation = () => {
         return (
             <div className="ph3 ph5-ns pv4 nowrap overflow-x-auto">
                 <NavLink to="/" className="link dim gray f5 f4-ns dib mr3">Home</NavLink>
-                <NavLink to="/our-history" className="link dim gray f5 f4-ns dib mr3">History</NavLink>
+                <NavLink to="/our-history" className="link dim gray f5 f4-ns dib mr3 selected" activeClassName="selected" activeStyle={{color: "red"}}>History</NavLink>
                 <NavLink to="/error" className="link dim gray f5 f4-ns dib mr3">Error page</NavLink>
             </div>
         );
